58db26f61de78456d56ebd7257062374f045c25d
gabin-ishimwe <g.ishimwe@alustudent.com>
2024-03-22 00:43:37 +0200

    new model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from model import load_model
 import pandas as pd
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 def calculate_days_difference(start_date, end_date):
     # Calculate the number of days between start and end dates
@@ -9,8 +10,8 @@ def calculate_days_difference(start_date, end_date):
     return delta.days
 
 def main():
-    st.title("Time series Auto-scaling app")
-    st.write("This app leverages machine learning to forecast the number of users and request count based on a user-input text and a selected date range. Simply enter some text, choose a start and end date, and click the 'Run Model' button to see the predictions.")
+    st.title("ML Auto-scaling app")
+    st.write("This app leverages machine learning to forecast the number of users, CPU utilization and request count based on the selected date range. Simply choose a start and end date, and click the 'Run Model' button to see the predictions.")
     # Set the date range to December of the current year
     current_year = datetime.now().year
 
@@ -45,9 +46,38 @@ def main():
             'Prediction': predictions.round(0)
         }
         predictions['timestamp'] = date_range
-        predictions = predictions.round(0)
-        st.dataframe(predictions.set_index('timestamp'))
+        predictions['CPU Utilization (%)'] = predictions['CPU Utilization (%)'].round(2)
+        predictions['number_of_users'] = predictions['number_of_users'].round(0)
+        predictions['request_count'] = predictions['request_count'].round(0)
+        # predictions = predictions.round(2)
+        # st.dataframe(predictions.set_index('timestamp'))
         print(predictions)
+        # Streamlit app
+
+        # Plot each column separately
+        st.subheader('Number of Users Over Time')
+        plt.figure(figsize=(10, 5))
+        plt.plot(predictions['timestamp'], predictions['number_of_users'], marker='o')
+        plt.xlabel('Date')
+        plt.ylabel('Number of Users')
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
+
+        st.subheader('Request Count Over Time')
+        plt.figure(figsize=(10, 5))
+        plt.plot(predictions['timestamp'], predictions['request_count'], marker='o', color='orange')
+        plt.xlabel('Date')
+        plt.ylabel('Request Count')
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
+
+        st.subheader('CPU Utilization Over Time')
+        plt.figure(figsize=(10, 5))
+        plt.plot(predictions['timestamp'], predictions['CPU Utilization (%)'], marker='o', color='green')
+        plt.xlabel('Date')
+        plt.ylabel('CPU Utilization (%)')
+        plt.xticks(rotation=45)
+        st.pyplot(plt)
 
 if __name__ == "__main__":
     main()
